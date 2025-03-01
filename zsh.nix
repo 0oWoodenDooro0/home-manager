d80afb1f63719a43e3373ffd16b3f0204d9fa05b
WoodenDoor <vincent031525@gmail.com>
2025-03-01 19:09:59 +0800

    change .zshrc

--- zsh.nix
+++ zsh.nix
@@ -8,7 +8,9 @@
     syntaxHighlighting.enable = true;
 
     initExtra = ''
-      export LANG=en_US.utf8
+      export LC_ALL=en_US.UTF-8
+      export LANG=en_US.UTF-8
+      export LC_TYPE=en_US.UTF-8
     '';
 
     antidote = {
