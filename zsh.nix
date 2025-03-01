7b17009d76829935f9690c382fe59c2323de39f3
WoodenDoor <vincent031525@gmail.com>
2025-03-01 18:38:11 +0800

    fix zsh.nix

--- zsh.nix
+++ zsh.nix
@@ -3,15 +3,15 @@
 {
   programs.zsh = {
     enable = true;
-    enableCompletion = true;
+#    enableCompletion = true;
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
 
-    initExtra = ''
-      export LC_ALL=C.UTF-8
-      export LANG=C.UTF-8
-      export LC_TYPE=C.UTF-8
-    '';
+#    initExtra = ''
+#      export LC_ALL=C.UTF-8
+#      export LANG=C.UTF-8
+#      export LC_TYPE=C.UTF-8
+#    '';
 
     antidote = {
       enable = true;
