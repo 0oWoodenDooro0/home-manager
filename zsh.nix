60653e6a5e7e762d6318052a578272959788ffbe
WoodenDoor <vincent031525@gmail.com>
2025-03-01 19:06:34 +0800

    change .zshrc

--- zsh.nix
+++ zsh.nix
@@ -4,14 +4,12 @@
   programs.zsh = {
     enable = true;
     enableCompletion = true;
-#    autosuggestion.enable = true;
+    autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
 
-#    initExtra = ''
-#      export LC_ALL=C.UTF-8
-#      export LANG=C.UTF-8
-#      export LC_TYPE=C.UTF-8
-#    '';
+    initExtra = ''
+      export LANG=en_US.utf8
+    '';
 
     antidote = {
       enable = true;
